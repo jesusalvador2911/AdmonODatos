43cc3f3b809346ed10bbe0f2e33a6513ff1deb17
jesusalvador2911 <61754948+jesusalvador2911@users.noreply.github.com>
2020-03-18 13:01:28 -0600

    Add files via upload

---
+++ 2.2/eje2.2.py
@@ -0,0 +1,8 @@
+f = open("poem.txt","r")
+print("1: ",f.read(4))
+print("2: ",f.readline())
+print("3: ",f.readline())
+print("4: ",f.readline())
+print("5: ",f.readline())
+print("6: ",f.readline())
+f.close()
