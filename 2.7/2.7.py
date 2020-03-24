967f9bcb6c006d33eaac464cb84609cc19eaf129
jesusalvador2911 <61754948+jesusalvador2911@users.noreply.github.com>
2020-03-24 11:38:14 -0600

    2.7

---
+++ 2.7/2.7.py
@@ -0,0 +1,8 @@
+f = open("poem_2.txt","w")
+f.write("When I think about myself, ")
+f.write("I almost laugh myself to death")
+f.close()
+f = open("poem_2.txt","r")
+data = f.read()
+print(data)
+f.close()
